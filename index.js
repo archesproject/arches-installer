6900c4b2344ae57637e4e88ee426a011c68ce925
Rob Gaston <rgaston@fargeo.com>
2016-01-05 12:15:39 -0800

    stubbing out main page based on latest mockups

---
+++ index.js
@@ -0,0 +1,13 @@
+var $ = require('jquery');
+var jQuery = $;
+require('pace');
+require('bootstrap');
+require('fastclick');
+require('nifty');
+require('switchery');
+require('bootstrap-select');
+require('chosen');
+
+$("#install-welcome").click(function(){
+		window.location.href = "http://127.0.0.1/arches41_mockups/install_wizard.html";
+});
